e1d9de316d5330af73a658d3fa564623449ee82a
Parthu <parthudhanireddy@gmail.com>
2026-08-25 18:01:48 +0530

    Fix Application configuration scanning and add spring-cloud-starter-config

--- src/main/java/com/fooddelivery/wallet/WalletServiceApplication.java
+++ src/main/java/com/fooddelivery/wallet/WalletServiceApplication.java
@@ -12,8 +12,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(
     scanBasePackages = {"com.fooddelivery.wallet", "com.fooddelivery.common"}
 )
-@org.springframework.boot.autoconfigure.domain.EntityScan(basePackages = {"com.fooddelivery.wallet", "com.fooddelivery.common.entity"})
-@org.springframework.data.jpa.repository.config.EnableJpaRepositories(basePackages = {"com.fooddelivery.wallet", "com.fooddelivery.common.repository"})
+@org.springframework.boot.autoconfigure.domain.EntityScan(basePackages = {"com.fooddelivery.wallet", "com.fooddelivery.common"})
+@org.springframework.data.jpa.repository.config.EnableJpaRepositories(basePackages = {"com.fooddelivery.wallet", "com.fooddelivery.common"})
 @EnableDiscoveryClient
 @EnableScheduling
 @ComponentScan(basePackages = {"com.fooddelivery.wallet", "com.fooddelivery.common"})
